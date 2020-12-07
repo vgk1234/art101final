3cc07683c5b5177434707bcb21f163bdb29ac811
vgk1234 <vinita.kulkarni@sjsu.edu>
2020-12-06 23:05:59 -0800

    Updated

--- sketch.js
+++ sketch.js
@@ -31,9 +31,6 @@ function results() {
         '<img src = "Assets/FriendB-Jordan.png" width="300" height ="300"> <br> <b>You would connect well with Jordan!</b> <br> <p> This person is very lowkey and chill; they really are invested in tech / gaming / geek culture and most often bond through online methods.</p>'
     } else if (friendC == friendA && friendC > friendB) {
         document.getElementById("results").innerHTML =
-        '<img src = "Assets/FriendC-Taylor.png" width="300" height ="300"> <b>You would connect well with Taylor!</b> <br> <p>This person loves forming close friendships and is ready to help anyone needed; they occasionally act as a sibling figure to their friends.</p>'        
-    } else if (friendA == friendC && friendA == friendB && friendB == friendC) {
-        document.getElementById("results").innerHTML =
-        '<img src = "Assets/FriendA-Sam.png" width="300" height ="300"> <br> <b>You would connect well with Sam!</b> <br> <p>This person often likes to simply talk to people in order to connect with them and is willing to engage in any way that involves excitement and energy!</p>' 
+        '<img src = "Assets/FriendC-Taylor.png" width="300" height ="300"> <br> <b>You would connect well with Taylor!</b> <br> <p>This person loves forming close friendships and is ready to help anyone needed; they occasionally act as a sibling figure to their friends.</p>'        
     }
 }
